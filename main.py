321c56055c2e981ba148b31636bf84f750429a04
Денис <DenisTachkasov19KP2@gmail.com>
2022-12-06 02:34:01 +0300

    Изменил файл main.py на ветке dev

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 
-def func(p, str_az, str_AZ):
+def func(p, str_az, str_AZ, str_09):
     for x in range(11):
-        p += ''.join(random.choices(str_az + str_AZ))
+        p += ''.join(random.choices(str_az + str_AZ + str_09))
     return p
 
 p = ''
 str_az = [chr(value) for value in range(ord('a'), ord('z'))]
 str_AZ = [chr(value) for value in range(ord('A'), ord('Z'))]
-res = func(p, str_az, str_AZ)
+str_09 = [str(value) for value in range(9)]
+res = func(p, str_az, str_AZ, str_09)
 print(res)
